82b080f4d9351833203d832fb92aad2baa2c5d6a
jessemok@yahoo.com <jessemok@yahoo.com>
2018-01-03 00:16:43 +0800

    New "Pause and Resume" function

--- userscript.js
+++ userscript.js
@@ -1,23 +1,32 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         QuickStock
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @version      0.2
+// @description  Enhance aastocks pages
+// @author       Jesse Mok
 // @match        http://www.aastocks.com/tc/stocks/analysis/company-fundamental/earnings-summary?symbol=*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    setTimeout(function(){ location.reload(); }, 10000);
+    window.timeoutVar = setTimeout(function(){ location.reload(); }, 10000);
     var s = '<div style="font-size: 22px">';
     s += '<a href="earnings-summary?symbol=00388">00388</a> ';
     s += '<a href="earnings-summary?symbol=00001">00001</a> ';
     s += '<a href="earnings-summary?symbol=00005">00005</a> ';
     s += '<a href="earnings-summary?symbol=00941">00941</a> ';
     s += '<a href="earnings-summary?symbol=00700">00700</a> ';
-    s += '<a href="earnings-summary?symbol=02628">02628</a>';
+    s += '<a href="earnings-summary?symbol=02628">02628</a> ';
+    s += '</div>';
+    s += '<div style="font-size: 18px">';
+    s += '<a href="#" id="monkeyControl" onclick="control()">Click to Pause</a>';
     s += '</div>';
     $(s).insertAfter('#cp_ucStockBar_pQuote');
+
+    window.control = function () {
+        clearTimeout(window.timeoutVar);
+        $("#monkeyControl").text("Click to Resume");
+        $("#monkeyControl").click(function () {location.reload();});
+    };
 })();
